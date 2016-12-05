0ce1527f51b6af9c197b1668d6a14fc18d306fea
thinkhy <think.hy@gmail.com>
2016-12-05 13:40:39 +0800

    resolved Intersection of Two Arrays II using Java

--- java/src/com/leetcode/Main.java
+++ java/src/com/leetcode/Main.java
@@ -4,13 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 	   System.out.println("Hello, leetcode");
-
 	   Solution s = new Solution();
+
+	   /*
 	   System.out.println(s.isPerfectSquare(10));
-        System.out.println(s.isPerfectSquare(16));
-        System.out.println(s.isPerfectSquare(14));
-        System.out.println(s.isPerfectSquare(2147483647));
+	   System.out.println(s.isPerfectSquare(16));
+	   System.out.println(s.isPerfectSquare(14));
+	   System.out.println(s.isPerfectSquare(2147483647));
+	   */
+
+	   int[] nums1 = {1, 2, 2, 1};
+	   int[] nums2 = {2, 2};
+
+	   int[] res = s.intersect(nums1, nums2);
+	   System.out.println(java.util.Arrays.toString(res));
+
+		nums1 = new int[]{};
+		nums2 = new int[]{1};
 
 
+		res = s.intersect(nums1, nums2);
+		System.out.println(java.util.Arrays.toString(res));
     }
 }
--- java/src/com/leetcode/Solution.java
+++ java/src/com/leetcode/Solution.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * Created by huangye on 11/28/16.
  */
@@ -21,4 +24,76 @@ public class Solution {
         }
         return false;
     }
+
+    public int[] intersection_hash(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        BitSet s = new BitSet();
+        BitSet interSet = new BitSet();
+
+        for (int i : nums1) {
+            s.set(i);
+        }
+
+        int m = 0;
+        for (int k : nums2) {
+            if (s.get(k) == true && interSet.get(k) == false) {
+                interSet.set(k);
+                m++;
+            }
+        }
+        int[] nums3 = new int[m];
+        int n = 0;
+        for (int i = 1; i <= interSet.size(); i++) {
+            if (interSet.get(i))
+                nums3[n++] = i;
+        }
+
+        return nums3;
+    }
+
+    /**
+     * 350. Intersection of Two Arrays II
+     * Refer to: https://leetcode.com/problems/intersection-of-two-arrays-ii/
+     *
+     * @param nums1   array of numbers
+     * @param nums2   array of numbers
+     * @return intersection of two arrays
+     */
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> m1 = new HashMap<>();
+        Map<Integer, Integer> m2 = new HashMap<>();
+
+        for (int k: nums1)
+            if (!m1.containsKey(k))
+                m1.put(k, 1);
+            else
+                m1.put(k, m1.get(k) + 1);
+
+        int m = 0;
+        for (int k: nums2) {
+            if (m1.containsKey(k) && m1.get(k) > 0) {
+                m++;
+                m1.put(k, m1.get(k)-1);
+
+                if (!m2.containsKey(k)) {
+                    m2.put(k, 1);
+                } else {
+                    m2.put(k, m2.get(k) + 1);
+                }
+            }
+        }
+
+        int[] res = new int[m];
+        int cnt = 0;
+
+        for (int k: m2.keySet())
+            for (int i = 0, n = m2.get(k); i < n; i++) {
+                res[cnt++] = k;
+            }
+
+        return res;
+    }
+
 }
